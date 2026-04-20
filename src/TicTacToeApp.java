6675821aa6d1a03d3f988bf5306c9d4acc9d012c
LAKSHYA-MITTAL14 <lakshyamittal14@gmail.com>
2026-04-20 15:20:45 +0530

    Tic-Tac-Toe/UC5

---
+++ src/TicTacToeApp.java
@@ -0,0 +1,28 @@
+public class TicTacToe {
+
+    static char[][] board = {
+        {'-', '-', '-'},
+        {'-', '-', '-'},
+        {'-', '-', '-'}
+    };
+
+    public static void main(String[] args) {
+        System.out.println(isValidMove(1, 1)); // true
+    }
+
+    // Validates move
+    static boolean isValidMove(int row, int col) {
+
+        // 1. Boundary check
+        if (row < 0 || row > 2 || col < 0 || col > 2) {
+            return false;
+        }
+
+        // 2. Cell empty check
+        if (board[row][col] != '-') {
+            return false;
+        }
+
+        return true;
+    }
+}
