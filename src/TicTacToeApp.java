2b07f11a87cf084228bc724849940e30720eec43
LAKSHYA-MITTAL14 <lakshyamittal14@gmail.com>
2026-04-20 15:01:25 +0530

    Tic-Tac-Toe/UC2

---
+++ src/TicTacToeApp.java
@@ -0,0 +1,41 @@
+import java.util.Random;
+
+public class TicTacToe {
+
+    static boolean isHumanTurn;
+    static char humanSymbol;
+    static char computerSymbol;
+
+    public static void main(String[] args) {
+        tossAndAssignSymbols();
+        displayTossResult();
+    }
+
+    // Performs toss and assigns symbols
+    static void tossAndAssignSymbols() {
+        Random random = new Random();
+        int toss = random.nextInt(2); // 0 or 1
+
+        if (toss == 0) {
+            isHumanTurn = true;
+            humanSymbol = 'X';
+            computerSymbol = 'O';
+        } else {
+            isHumanTurn = false;
+            humanSymbol = 'O';
+            computerSymbol = 'X';
+        }
+    }
+
+    // Displays result of toss
+    static void displayTossResult() {
+        if (isHumanTurn) {
+            System.out.println("Human won the toss and will play first.");
+        } else {
+            System.out.println("Computer won the toss and will play first.");
+        }
+
+        System.out.println("Human Symbol: " + humanSymbol);
+        System.out.println("Computer Symbol: " + computerSymbol);
+    }
+}
